9360456ba9fa0c84f65525ed37d5665bfb55d7a4
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2022-01-25 23:55:42 +0530

    Entry point to all files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+/*export{ default as SearchBar } from './SearchBar';
+export{ default as VideoDetail } from './VideoDetail'
+export{ default as VideoList } from './VideoList';*/
+
+
+ReactDOM.render(
+    <App />,
+  document.querySelector('#root')
+);
+
+
